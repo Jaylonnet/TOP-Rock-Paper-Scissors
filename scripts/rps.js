f1c411af661801ec378c8c5a3dfc9d3605de6d4b
Jaylon <jaylonwingfield10@gmail.com>
2022-03-29 15:47:31 +0100

    Logic complete

--- scripts/rps.js
+++ scripts/rps.js
@@ -1,13 +1,21 @@
 const roundWinner = document.querySelector('#round-winner');
+const computerChoice = document.querySelector('#computer .choice')
+const playerChoice = document.querySelector('#player .choice')
+const playerScore = document.querySelector('#player-score')
+const computerScore = document.querySelector('#computer-score')
+const drawScore = document.querySelector('#draw-score')
+
 function computerPlay() {
     const rpsGuesses = ['Rock', 'Paper', 'Scissors'];
     let guess = rpsGuesses[Math.floor(Math.random() * rpsGuesses.length)];
     return guess
-}
+};
 
 function playRound(playerSelection) {
     let computerSelection = computerPlay();
-    
+    computerChoice.textContent = computerSelection;
+    playerChoice.textContent = playerSelection;
+
     let winner = '';
 
     if (computerSelection == playerSelection) {
@@ -32,10 +40,30 @@ function playRound(playerSelection) {
         winner = 'Computer';
     }
     declareRoundWinner(winner);
-}
+};
 
 function declareRoundWinner(winner) {
     roundWinner.textContent = `${winner}`
+    if (winner == 'Player') {
+        playerScore.textContent = Number(playerScore.textContent) + 1
+    }
+    else if (winner == 'Computer') {
+        computerScore.textContent = Number(computerScore.textContent) + 1
+    }
+    else if (winner == 'Draw') {
+        drawScore.textContent = Number(drawScore.textContent) + 1
+    };
+    checkGameActive();
+};
+
+function checkGameActive() {
+    if (playerScore.textContent == '5') {
+        roundWinner.textContent = 'Player won this game!';
+        disableGame()
+    } else if (computerScore.textContent == '5') {
+        roundWinner.textContent = 'Computer won this game!';
+        disableGame()
+    }
 }
 
 // event listener added to RPS buttons, which then plays a round
@@ -45,3 +73,9 @@ buttons.forEach((button) => {
         playRound(button.textContent);
     });
 });
+
+function disableGame() {
+    buttons.forEach((button) => {
+        button.disabled = true
+    })
+}
